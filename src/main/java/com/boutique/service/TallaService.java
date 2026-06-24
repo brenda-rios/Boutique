27dfa6ef722758a60336f1b7b02cf41724702a8e
Brenda Rios <brersvg@gmail.com>
2026-06-23 20:49:12 -0600

    Inicialización de html

--- src/main/java/com/boutique/service/TallaService.java
+++ src/main/java/com/boutique/service/TallaService.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.boutique.dto.TallaDTO;
 import com.boutique.model.Talla;
 import com.boutique.repo.TallaRepo;
 
+@Service
 public class TallaService {
 	@Autowired
     TallaRepo tallaRepo;
