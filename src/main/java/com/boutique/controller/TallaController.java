1070281f6f4065e71de5e3a862e42efb45d8a6b0
LizbethCL <lizc2025lopez@gmail.com>
2026-06-24 17:54:18 -0600

    Cambios en catalogo Tallas

--- src/main/java/com/boutique/controller/TallaController.java
+++ src/main/java/com/boutique/controller/TallaController.java
@@ -1,11 +1,14 @@
 package com.boutique.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -40,4 +43,27 @@ public class TallaController {
         tallaService.guardar(talla);
         return "redirect:/rutaTallas/listar";
     }
+    @PostMapping("actualizar")
+	public String metodoActualizar(@Valid @ModelAttribute("talla") TallaDTO talla, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			model.addAttribute("uuid", talla.getUuid());
+			return "/carpetaTallas/paginaFormulario";
+		}
+		tallaService.actualizar(talla);
+		return "redirect:/rutaTallas/listar";
+	}
+	
+	@GetMapping("editar/{uuid}")
+	public String metodoEditar(Model model, @PathVariable UUID uuid) {
+		model.addAttribute("talla", tallaService.obtenerTallaUUID(uuid));
+		model.addAttribute("uuid", uuid);
+		return "/carpetaTallas/paginaFormulario";
+	}
+	
+	@GetMapping("eliminar/{uuid}")
+	public String metodoEliminar(Model model, @PathVariable UUID uuid) {
+		tallaService.borrar(uuid);
+		return "redirect:/rutaTallas/listar";
+	}
+
 	}
--- src/main/java/com/boutique/service/TallaService.java
+++ src/main/java/com/boutique/service/TallaService.java
@@ -1,6 +1,8 @@
 package com.boutique.service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Service;
 import com.boutique.dto.TallaDTO;
 import com.boutique.model.Talla;
 import com.boutique.repo.TallaRepo;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class TallaService {
@@ -26,5 +30,31 @@ public class TallaService {
     public void guardar(TallaDTO talla) {
         tallaRepo.save(mapper.map(talla, Talla.class));
     }
-
+    public void actualizar(TallaDTO talla) {
+		Optional<Talla> OptTalla = tallaRepo.findByUuid(talla.getUuid());
+		if (OptTalla.isPresent()) {
+			mapper.map(talla, OptTalla.get());
+			tallaRepo.save(OptTalla.get());
+		} else {
+			throw new EntityNotFoundException("Talla no encontrada con el UUID: " + talla.getUuid());
+		}	
+	}
+	
+	public void borrar(UUID uuid) {
+		Optional<Talla> OptTalla = tallaRepo.findByUuid(uuid);
+		if (OptTalla.isPresent()) {
+			tallaRepo.delete(OptTalla.get());
+		} else {
+			throw new EntityNotFoundException("Talla no encontrada con el UUID: " + uuid);
+		}
+	}
+	
+	public TallaDTO obtenerTallaUUID(UUID uuid) {
+		Optional<Talla> OptTalla = tallaRepo.findByUuid(uuid);
+		if (OptTalla.isPresent()) {
+			return mapper.map(OptTalla.get(), TallaDTO.class);
+		} else {
+			throw new EntityNotFoundException("Talla no encontrada con el UUID: " + uuid);
+		}
+	}
 }
